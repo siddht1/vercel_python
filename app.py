5ba546ed68ddeee841bca197b3b01ee8e9b6f0e2
siddht1 <rt1yrru+github@gmail.com>
2023-02-24 21:53:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, jsonify, request
-import pyrebase
-import uuid
-import datetime
-import socket
-import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
 
-print(uuid)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        message = 'Hello, World!'
+        self.wfile.write(message.encode())
+
+def run():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, Handler)
+    print('Server running at localhost:8000')
+    httpd.serve_forever()
+
+run()
